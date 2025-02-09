b2b593f76593b7a08c9152afc72c40da92ebcc04
Erik Persson <erik.pers03@gmail.com>
2025-02-09 20:42:56 +0100

    fix/feat: init structure

---
+++ delalive/src/app/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+import "./Header.css"
+
+export default function Header(){
+
+    return(
+        <header>
+            <h1 className="header-h1">
+                <p className="header-title">
+                    Dela Live
+                </p>
+            </h1>
+        </header>
+    );
+}
